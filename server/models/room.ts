1868d4be7cc390e1f6a3eb36cfac4235451c5127
Jerry <szy0syz@gmail.com>
2021-01-29 16:16:15 +0800

    Update create room model

--- server/models/room.ts
+++ server/models/room.ts
@@ -1,39 +1,3 @@
-# tfjs-object-detection
-
-> Object Detect with TensorFlow, React, Mongo, TS, Redis, WS
-
-## Note
-
-- `/server`
-- `npm init -y` `tsc --init`
-- `npm i typescript express @types/typescript @types/express axios @types/axios nodemon @types/nodemon`
-- `npm i -D ts-node`
-
-```ts
-import express from "express";
-import bodyParser from "body-parser";
-import { indexRouter } from "./routes";
-
-const app = express();
-app.use(bodyParser.json({ type: "*/*" }));
-
-app.get("/api/ping", (_, res) => {
-  res.status(200).send({ pong: true });
-});
-
-app.use(indexRouter);
-
-app.get("*", (_, res) => {
-  res.status(400).send({ error: "Not Found" });
-});
-
-export { app };
-```
-
-- `npm i mongoose @types/mongoose`
-- 正儿八经的 `ts` 版 `mongoose` 的 `model` 建立规范
-
-```ts
 import mongoose from 'mongoose';
 
 interface RoomAttrs {
@@ -87,5 +51,3 @@ roomSchema.statics.build = (attrs: RoomAttrs) => {
 const Room = mongoose.model<RoomDoc, RoomModel>('Room', roomSchema)
 
 export { Room }
-
-```
